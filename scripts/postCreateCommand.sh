dce1c49ea0ba273f12f9e4269abd4ba027d1c028
MRyutaro <m2002r1028@icloud.com>
2024-12-10 16:52:11 +0000

    add: init

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -2,3 +2,5 @@ pip install -r requirements.txt
 
 # https://github.com/MRyutaro/elastic_notebook_slim.gitをpip installする
 pip install git+https://github.com/MRyutaro/elastic_notebook_slim.git
+
+npm install -g configurable-http-proxy
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,6 @@
 # ========================================
 # 変数
 PORT=8888
-JUPYTER_TOKEN=2ab300dd6bbb0b399187272848dd4e4de064ca7e95b65f4b
 # ========================================
 
 # 現在のパスを取得
@@ -13,7 +12,7 @@ KERNELS_DIR=/home/$USRE_NAME/.local/share/jupyter/kernels
 echo ROOT_DIR: $ROOT_DIR
 echo KERNELS_DIR: $KERNELS_DIR
 
-CONFIG_FILE=$ROOT_DIR/jupyter_lab_config.py
+CONFIG_FILE=$ROOT_DIR/jupyterhub_config.py
 echo CONFIG_FILE: $CONFIG_FILE
 
 # KERNELS_DIRがなかったら作成
@@ -45,10 +44,5 @@ done
 
 jupyter kernelspec list
 
-jupyter-lab \
-    --no-browser \
-    --ip=0.0.0.0 \
-    --port=$PORT \
-    --port-retries=0 \
-    --config=$CONFIG_FILE \
-    --NotebookApp.token=$JUPYTER_TOKEN  \
+jupyterhub \
+    -f $CONFIG_FILE \
