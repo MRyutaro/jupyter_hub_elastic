bcd1709b6ceacd6194f652fd0a9a6a7fcb0db6c5
MRyutaro <m2002r1028@icloud.com>
2024-12-10 17:03:35 +0000

    fix

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -7,12 +7,12 @@ c = get_config() # type: ignore
 # http://localhost:8000
 # にアクセスする
 
-c.JupyterHub.active_server_limit = 3
+c.JupyterHub.active_server_limit = 5
 c.JupyterHub.authenticator_class = 'jupyterhub.auth.DummyAuthenticator'
 c.JupyterHub.allow_named_servers = True
 c.JupyterHub.ip = '0.0.0.0'
 c.JupyterHub.port = 8000
 c.Spawner.default_url = '/lab'
 c.Spawner.notebook_dir = '/workspaces/jupyter_hub_elastic'
-c.Authenticator.allowed_users = set("vscode")
-c.Authenticator.admin_users = set("vscode")
+c.Authenticator.allowed_users = set(['vscode'])
+c.Authenticator.admin_users = set(['vscode'])
