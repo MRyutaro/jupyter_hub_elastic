67a60628d862ca230c9fb6f865cc436ffbce5617
MRyutaro <m2002r1028@icloud.com>
2024-12-11 09:18:43 +0000

    update: リスタート

--- scripts/restart_server.py
+++ scripts/restart_server.py
@@ -15,14 +15,27 @@ if API_TOKEN is None:
 
 api_url = 'http://127.0.0.1:8081/hub/api'
 
-user_name = 'user01'
+user_name = 'vscode'
 server_name = ''
 
+# サーバーの停止
 r = requests.delete(
-    api_url + f'/users/{user_name}/servers/{server_name}',
+    f'{api_url}/users/{user_name}/servers/{server_name}',
     headers={
         'Authorization': f'token {API_TOKEN}'
     }
 )
 
 r.raise_for_status()
+# 戻り値はない
+
+# サーバーの再起動
+r = requests.post(
+    f'{api_url}/users/{user_name}/servers/{server_name}',
+    headers={
+        'Authorization': f'token {API_TOKEN}'
+    }
+)
+
+r.raise_for_status()
+# 戻り値はない
